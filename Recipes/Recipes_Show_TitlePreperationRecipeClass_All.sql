22a2e9386dfcc4e6f313603ade2b2bf5652ba939
Wes Hediger <wes.hediger@gmail.com>
2017-01-13 13:54:36 -0700

    add qualify names

--- Recipes/Recipes_Show_TitlePreperationRecipeClass_All.sql
+++ Recipes/Recipes_Show_TitlePreperationRecipeClass_All.sql
@@ -1,5 +1,5 @@
 
-SELECT RecipeTitle, Preparation, RecipeClassDescription
+SELECT Recipes.RecipeTitle, Recipes.Preparation, Recipe_Classes.RecipeClassDescription
 FROM Recipe_Classes
 INNER JOIN Recipes
 ON Recipe_Classes.RecipeClassID = Recipes.RecipeClassID;
