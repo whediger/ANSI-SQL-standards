9ff5ded460a326f191c284f68c8463684dda721a
Wes Hediger <wes.hediger@gmail.com>
2017-01-15 22:51:47 -0700

    add Customers_Vendors_Employees__Mailing_List

---
+++ sales_orders/Customers_Vendors_Employees_Mailing_List.sql
@@ -0,0 +1,19 @@
+
+SELECT CONCAT(Customers.CustFirstName, ' ', Customers.CustLastName) AS Fullname,
+      Customers.CustStreetAddress, Customers.CustCity,
+      Customers.CustState, Customers.CustZipCode
+FROM Customers
+
+UNION
+
+SELECT CONCAT(Employees.EmpFirstName, ' ', Employees.EmpLastName) AS FullName,
+      Employees.EmpStreetAddress, Employees.EmpCity,
+      Employees.EmpState, Employees.EmpZipCode
+FROM Employees
+
+UNION
+
+SELECT Vendors.VendName AS FullName,
+      Vendors.VendStreetAddress, Vendors.VendCity,
+      Vendors.VendState, Vendors.VendZipCode
+FROM Vendors;
