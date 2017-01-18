e04956fbcbd3a2fbff11aabba617272b687f468a
Wes Hediger <wes.hediger@gmail.com>
2017-01-17 23:16:47 -0700

    refactor - use NOT IN

--- school_scheduling/Students_Never_Withdrawn.sql
+++ school_scheduling/Students_Never_Withdrawn.sql
@@ -1,13 +1,13 @@
 
-SELECT CONCAT(Students.StudLastName, ', ', Students.StudFirstName) AS Student_Full_Name
-FROM Students
-LEFT OUTER JOIN
-  (SELECT Student_Schedules.StudentID
-  FROM Student_Class_Status
-  INNER JOIN Student_Schedules
-    ON  Student_Class_Status.ClassStatus =
-        Student_Schedules.ClassStatus
-  WHERE Student_Class_Status.ClassStatusDescription = 'withdrew')
-  AS Withdrew
-ON Students.StudentID = Withdrew.StudentID
-WHERE Withdrew.StudentID IS NULL;
+SELECT  Students.StudentID,
+        Students.StudFirstName,
+        Students.StudLastName
+FROM  Students
+WHERE Students.StudentID
+      NOT IN
+      (SELECT Student_Schedules.StudentID
+      FROM  Student_Schedules
+      INNER JOIN Student_Class_Status
+        ON  Student_Schedules.ClassStatus =
+            Student_Class_Status.ClassStatus
+      WHERE Student_Class_Status.ClassStatusDescription = 'Withdrew');
