f6e81250f0ee999215fd4af3d243efa057bf0c10
Wes Hediger <wes.hediger@gmail.com>
2017-01-19 05:58:01 -0700

    add Ingredients_Using_NonStandard_Measure

---
+++ Recipes/Ingredients_Using_NonStandard_Measure.sql
@@ -0,0 +1,13 @@
+
+SELECT DISTINCT Ingredients.IngredientName
+FROM Ingredients
+INNER JOIN Recipe_Ingredients
+  ON  Ingredients.IngredientID =
+      Recipe_Ingredients.IngredientID
+WHERE Recipe_Ingredients.MeasureAmountID =
+      SOME(SELECT RI.MeasureAmountID
+          FROM Recipe_Ingredients AS RI
+          INNER JOIN Measurements
+            ON  RI.MeasureAmountID =
+                Measurements.MeasureAmountID
+          WHERE RI.MeasureAmountID <> Recipe_Ingredients.MeasureAmountID);
