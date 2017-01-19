113b8aa8657fe9501be87746a29c8f185921a85f
Wes Hediger <wes.hediger@gmail.com>
2017-01-19 07:08:30 -0700

    refactor

--- Bowling-league/Bowlers_Low_Score.sql
+++ Bowling-league/Bowlers_Low_Score.sql
@@ -1,18 +1,14 @@
 
-SELECT DISTINCT CONCAT(Bowlers.BowlerFirstName, ' ', Bowlers.BowlerLastName) AS BowlerLastName,
-    Teams.TeamName
-FROM (Bowlers
-INNER JOIN Teams
-  ON  Bowlers.TeamID =
-      Teams.TeamID)
-INNER JOIN Bowler_Scores
-  ON  Bowler_Scores.BowlerID =
-      Bowlers.BowlerID
-WHERE Bowler_Scores.RawScore < ALL
-      (SELECT BS2.RawScore
-      FROM Bowlers AS B2
-      INNER JOIN Bowler_Scores AS BS2
-        ON  B2.BowlerID =
-            BS2.BowlerID
-      WHERE B2.BowlerID <> Bowlers.BowlerID
-      AND   B2.TeamID = Bowlers.TeamID);
+SELECT DISTINCT CONCAT(Bowlers.BowlerFirstName, ' ', Bowlers.BowlerLastName) AS LowBowler
+FROM Bowlers
+INNER JOIN  Bowler_Scores
+  ON  Bowlers.BowlerID =
+      Bowler_Scores.BowlerID
+WHERE Bowler_Scores.RawScore <
+      ALL(SELECT BS2.RawScore
+          FROM Bowlers AS B2
+          INNER JOIN Bowler_Scores AS BS2
+            ON  B2.BowlerID =
+                BS2.BowlerID
+            WHERE B2.BowlerID <> Bowlers.BowlerID
+            AND   Bowlers.TeamID = B2.TeamID);
