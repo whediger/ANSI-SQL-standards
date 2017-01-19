00ca449a34a2fb37b1802771e63f5e0334b22989
Wes Hediger <wes.hediger@gmail.com>
2017-01-19 00:27:41 -0700

    refactor

--- Bowling-league/Bowlers_And_Count_Games.sql
+++ Bowling-league/Bowlers_And_Count_Games.sql
@@ -3,8 +3,6 @@ SELECT CONCAT(Bowlers.BowlerFirstName, ' ', Bowlers.BowlerLastName) AS FullName,
       (SELECT COUNT(*)
       FROM  Bowler_Scores
       WHERE Bowler_Scores.BowlerID =
-            Bowlers.BowlerID
-      AND   Bowler_Scores.GameNumber IS NOT NULL)
+            Bowlers.BowlerID)
       AS Game_Count
-FROM Bowlers
-WHERE Bowlers.BowlerLastName = 'Pundt'; 
+FROM Bowlers;
