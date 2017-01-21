0fa3a4a1c249fa4d2dc0876711dfd50866bac45e
Wes Hediger <wes.hediger@gmail.com>
2017-01-20 20:54:35 -0700

    add Entertainers with no contracts

--- entertaiment_agency/Aggregate_Contract_Info_By_entertainer.sql
+++ entertaiment_agency/Aggregate_Contract_Info_By_entertainer.sql
@@ -1,12 +1,12 @@
 
 SELECT  Entertainers.EntStageName,
-        COUNT(*) AS NumContracts,
+        COUNT(Engagements.EntertainerID) AS NumContracts,
         SUM(Engagements.ContractPrice) AS TotPrice,
         MIN(Engagements.ContractPrice) AS MinPrice,
         MAX(Engagements.ContractPrice) AS MaxPrice,
         AVG(Engagements.ContractPrice) AS AvgPrice
 FROM Entertainers
-INNER JOIN Engagements
+LEFT OUTER JOIN Engagements
   ON  Entertainers.EntertainerID =
       Engagements.EntertainerID
 GROUP BY Entertainers.EntStageName;
