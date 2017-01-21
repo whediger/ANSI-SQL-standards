2ffdf1fad9adc2a9717ff4f4108fdd4a36c57d34
Wes Hediger <wes.hediger@gmail.com>
2017-01-21 15:47:43 -0700

    playing around, saving changes

--- Recipes/Meat_Ingredient_Recipe_Count_Group.sql
+++ Recipes/Meat_Ingredient_Recipe_Count_Group.sql
@@ -1,9 +1,14 @@
 
 SELECT  Ingredients.IngredientName,
-        COUNT(Recipe_Ingredients.IngredientID) AS Recipe_Count
+        (SELECT COUNT(Recipe_Ingredients.IngredientID) AS Recipe_Count
+        FROM Recipe_Ingredients
+        WHERE Recipe_Ingredients.IngredientID =
+              Ingredients.IngredientID)
+        AS Recipe_Count
 FROM Ingredients
 INNER JOIN Recipe_Ingredients
   ON  Ingredients.IngredientID =
       Recipe_Ingredients.IngredientID
 WHERE Ingredients.IngredientClassID = 2
-GROUP BY Ingredients.IngredientName;
+GROUP BY  Ingredients.IngredientName,
+          Recipe_Count;
