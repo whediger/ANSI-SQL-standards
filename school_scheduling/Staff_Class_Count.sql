59316efc3880b805584544beaaae0a1b06924856
Wes Hediger <wes.hediger@gmail.com>
2017-01-20 23:41:41 -0700

    add staff with no classes

--- school_scheduling/Staff_Class_Count.sql
+++ school_scheduling/Staff_Class_Count.sql
@@ -2,7 +2,7 @@
 SELECT CONCAT(Staff.StfFirstName, ' ', Staff.StfLastName) AS Staff_Full_Name,
     COUNT(Faculty_Classes.ClassID) AS Num_Of_Classes
 FROM Staff
-INNER JOIN Faculty_Classes
+LEFT OUTER JOIN Faculty_Classes
   ON  Staff.StaffID =
       Faculty_Classes.StaffID
 GROUP BY Staff_Full_Name;
