f7880f8370daa9e0ecb8fbf5c7e8427c7d458ca4
Wes Hediger <wes.hediger@gmail.com>
2017-01-23 16:24:57 -0700

    add Captains_Who_Are_Hotshots

---
+++ Bowling-league/Captains_Who_Are_Hotshots.sql
@@ -0,0 +1,28 @@
+
+SELECT CONCAT(Bowlers.BowlerFirstName, ' ', Bowlers.BowlerLastName) AS Captains_Name,
+      MAX(Bowler_Scores.RawScore) AS Personal_Record,
+      Team_Scores.T_High_Score
+FROM Bowlers
+INNER JOIN Teams
+  ON  Bowlers.BowlerID =
+      Teams.CaptainID
+INNER JOIN Bowler_Scores
+  ON  Teams.CaptainID =
+      Bowler_Scores.BowlerID
+INNER JOIN (SELECT  MAX(BS.RawScore) AS T_High_Score,
+                    T.TeamID
+            FROM Teams AS T
+            INNER JOIN Bowlers AS B
+              ON  T.TeamID =
+                  B.TeamID
+            INNER JOIN Bowler_Scores AS BS
+              ON  B.BowlerID =
+                  BS.BowlerID
+            GROUP BY  T.TeamID)
+            AS Team_Scores
+  ON  Team_Scores.TeamID =
+      Teams.TeamID
+GROUP BY  Captains_Name,
+          Team_Scores.T_High_Score
+HAVING  Personal_Record
+        < Team_Scores.T_High_Score;
