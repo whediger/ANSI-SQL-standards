73d9d4233f85bfa934ddac35403c03e5ee50a683
Wes Hediger <wes.hediger@gmail.com>
2017-01-22 17:31:41 -0700

    add Entertainers_MoreThan_2_Overlap

---
+++ entertaiment_agency/Entertainers_MoreThan_2_Overlap.sql
@@ -0,0 +1,34 @@
+
+SELECT Overlapped.EntStageName AS Ent_Overlapped_More_Than_2_Times
+FROM
+    (SELECT  Entertainers.EntStageName,
+              Eng1.EngagementNumber,
+              Eng1.StartDate,
+              Eng1.EndDate,
+              Eng1.StartTime,
+              Eng1.StopTime
+    FROM Entertainers
+    INNER JOIN Engagements AS Eng1
+      ON  Entertainers.EntertainerID =
+          Eng1.EntertainerID
+    INNER JOIN Engagements AS Eng2
+      ON  Eng1.EntertainerID =
+          Eng2.EntertainerID
+    WHERE Eng1.EngagementNumber != Eng2.EngagementNumber
+    AND   Eng2.StartDate <=
+          Eng1.EndDate
+    AND   Eng2.EndDate >=
+          Eng1.StartDate
+    AND   Eng2.StartTime <=
+          Eng1.StopTime
+    AND   Eng2.StopTime >=
+          Eng1.StartTime
+    GROUP BY  Eng1.EngagementNumber,
+              Entertainers.EntStageName,
+              Eng1.StartDate,
+              Eng1.EndDate,
+              Eng1.StartTime,
+              Eng1.StopTime)
+    AS Overlapped
+GROUP BY Overlapped.EntStageName
+HAVING COUNT(Overlapped.EntStageName) > 2;
