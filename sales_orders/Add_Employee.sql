244efb48242cd0d7ab61ae09416251e8317574a8
Wes Hediger <wes.hediger@gmail.com>
2017-01-25 12:27:10 -0700

    add Add_Employee

---
+++ sales_orders/Add_Employee.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Employees
+      (EmpFirstName, EmpLastName,
+      EmpStreetAddress, EmpCity, EmpState,
+      EmpZipCode, EmpAreaCode, EmpPhoneNumber)
+VALUES ('Susan', 'Metters',
+        '16547 NE 132nd St', 'Woodinville', 'WA',
+        '98072', 425, '555-7825');
