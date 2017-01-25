6209aedb387a0956d255d1610c50ae4b21dec7c6
Wes Hediger <wes.hediger@gmail.com>
2017-01-25 13:42:36 -0700

    add Add_New_Accounting_Class

---
+++ school_scheduling/Add_New_Accounting_Class.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Classes
+      (SubjectID, ClassRoomID, Credits, StartDate, StartTime,
+      Duration, TuesdaySchedule, ThursdaySchedule)
+VALUES (4, 3315, 5, '2014-01-14', '15:00:00',
+        80, 1, 1);
