8c72f37933dbd300e6e2bde0b35e669927a80ed8
Wes Hediger <wes.hediger@gmail.com>
2017-01-25 13:06:20 -0700

    add Add_Vendor

---
+++ sales_orders/Add_Vendor.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO Vendors
+      (VendName, VendStreetAddress, VendCity,
+      VendState, VendZipCode, VendPhoneNumber,
+      VendFaxNumber, VendWebPage,
+      VendEmailAddress)
+VALUES ('Hot Dog Bikes', '1234 Main Street', 'Chicago',
+        'IL', '60620', '(773) 555-1212',
+        '(773) 555-2341', 'http://www.hotdogbikes.com/',
+        'Sales@hotdogbikes.com');
