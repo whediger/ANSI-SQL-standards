d003de99be1adb0004723c8039c8d5cadbf573aa
Wes Hediger <wes.hediger@gmail.com>
2017-01-25 13:19:24 -0700

    add Add_Customer

---
+++ entertaiment_agency/Add_Customer.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Customers
+      (CustFirstName, CustLastName,
+      CustStreetAddress, CustCity, CustState,
+      CustZipCode, CustPhoneNumber)
+VALUES ('Ann', 'Hernandez',
+        '457 211th St NE', 'Bothell', 'WA',
+        '98200', '555-3945');
