e71534ed81e4eac13a3993d377d1b8a6e96e35ec
Wes Hediger <wes.hediger@gmail.com>
2017-01-25 19:01:19 -0700

    add Add_Customer

---
+++ sales_orders/Add_Customer.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO Customers
+      (CustFirstName,
+      CustLastName,
+      CustStreetAddress,
+      CustCity,
+      CustState,
+      CustZipCode,
+      CustAreaCode,
+      CustPhoneNumber)
+VALUES ('Mary',
+        'Baker',
+        '7834 W 32nd Ct.',
+        'Bothel',
+        'WA',
+        '98011',
+        425,
+        '555-9876');
