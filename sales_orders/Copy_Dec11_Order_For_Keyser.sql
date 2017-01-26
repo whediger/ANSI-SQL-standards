d35eb0c9c02bc0a0dd926c73558cd60d48b0d339
Wes Hediger <wes.hediger@gmail.com>
2017-01-25 17:17:44 -0700

    add Copy_Dec11_Order_For_Keyser

---
+++ sales_orders/Copy_Dec11_Order_For_Keyser.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO Orders
+      (OrderDate, ShipDate, CustomerID,
+      EmployeeID, OrderTotal)
+SELECT  DISTINCT '2013-06-12',
+        '2013-06-15',
+        Orders.CustomerID,
+        Orders.EmployeeID,
+        Orders.OrderTotal
+FROM Customers
+INNER JOIN Orders
+  ON  Customers.CustomerID =
+      Orders.CustomerID
+INNER JOIN Order_Details
+  ON  Orders.OrderNumber =
+      Order_Details.OrderNumber
+WHERE Orders.OrderDate = '2012-12-11'
+AND   Customers.CustLastName = 'Keyser';
